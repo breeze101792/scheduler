b412c0aa8a6f71b8cd646df09a53efbe98d2db1f
Shaowu Tseng <breeze101792@gmail.com>
2022-04-09 19:02:13 +0800

    [CLI] Add autocomplete support
    [Description]
    Add autocomplete support
    
    [Self-Test]
    N/A

--- utility/cli.py
+++ utility/cli.py
@@ -29,16 +29,16 @@ class ArgParser:
     def __str__(self):
         print(self.__target_keys)
     def __getitem__(self, key):
-        dbg_debug("__getitem__")
+        # dbg_debug("__getitem__")
         if self.__keytransform__(key) is None:
-            dbg_debug("Return None")
+            # dbg_debug("Return None")
             return None
         else:
             return self.__args_dict[self.__keytransform__(key)]
     def __keytransform__(self, key):
-        dbg_debug("__keytransform__")
+        # dbg_debug("__keytransform__")
         if key not in self.keys():
-            dbg_debug("Return None")
+            # dbg_debug("Return None")
             return None
         else:
             return key
@@ -84,6 +84,42 @@ class ArgParser:
         # print(arg_dict)
         return arg_dict
 
+class CommandInstance:
+    def __init__(self, key_word, func_ptr, description="", arg_list=None):
+        # key_word, func_ptr, description="", arg_list=l
+        self.__key_word    = key_word
+        self.__func_ptr    = func_ptr
+        self.__description = description
+        self.__arg_list    = arg_list
+
+    @property
+    def key_word(self):
+        return self.__key_word
+    @key_word.setter
+    def key_word(self,val):
+        self.__key_word = val
+
+    @property
+    def func_ptr(self):
+        return self.__func_ptr
+    @func_ptr.setter
+    def func_ptr(self,val):
+        self.__func_ptr = val
+
+    @property
+    def description(self):
+        return self.__description
+    @description.setter
+    def description(self,val):
+        self.__description = val
+
+    @property
+    def arg_list(self):
+        return self.__arg_list
+    @arg_list.setter
+    def arg_list(self,val):
+        self.__arg_list = val
+
 class CommandLineInterface:
     def __init__(self, promote="cli"):
         #### control vars ###
@@ -97,47 +133,49 @@ class CommandLineInterface:
         self.__auto_match = True
 
         ### Function Configs ###
-        self.regist_cmd("help", self.__help, "Print help")
         self.regist_cmd("exit", self.__exit, "Exit the program")
+        self.regist_cmd("help", self.__help, "Print help")
         self.regist_cmd("history", self.__hist, "Print history")
     def __exit(self, args):
-        self.ui_print_line("Exit program")
+        self.print("Exit program")
         exit()
     def __hist(self, args):
-        self.ui_print_line("History")
+        self.print("History")
         # for each_cmd in self.__history_list:
-        #     self.ui_print_line(" " + each_cmd)
+        #     self.print(" " + each_cmd)
         for each_idx in range(0, len(self.__history_list)):
-            self.ui_print_line("% 4d. %s" % (each_idx,  self.__history_list[each_idx]))
+            self.print("% 4d. %s" % (each_idx,  self.__history_list[each_idx]))
         return True
 
     def __help(self, args):
-        self.ui_print_line("Help")
+        self.print("Help")
         # print("   under construction")
         for each_key in self.__function_dict.keys():
-            self.ui_print_line("  %- 8s: %s" % (each_key, self.__function_dict[each_key][1]))
+            self.print("  %- 8s: %s" % (each_key, self.__function_dict[each_key].description))
         return True
+    # @staticmethod
+    # def print(*args):
+    #     timestamp = strftime("%d-%H:%M", gmtime())
+    #     print("[{}]".format(timestamp) + "".join(map(str,args)))
     def set_mode(self, mode):
         self.__mode = mode
 
     @staticmethod
-    def ui_print(*args):
-        print("".join(map(str,args)), end="", flush=True)
-    @staticmethod
-    def ui_print_line(*args):
-        print("".join(map(str,args)), flush=True)
+    def print(*args, end="\n"):
+        print("".join(map(str,args)), end=end, flush=True)
+    # @staticmethod
+    # def println(*args):
+    #     print("".join(map(str,args)), flush=True)
     def __print_line_buffer(self, line_buffer, cursor_shift_idx):
         columns, rows = os.get_terminal_size(0)
         trailing_space_nmu=columns - len("\r"+self.__promote+line_buffer)
-        # self.ui_print("\r"+" "*(len(self.__promote)+len(line_buffer)))
-        # self.ui_print("\r                                             ")
-        self.ui_print("\r"+self.__promote+line_buffer+trailing_space_nmu*" ")
-        self.ui_print("\033[%dD" % (cursor_shift_idx + trailing_space_nmu))
+        # self.print("\r"+" "*(len(self.__promote)+len(line_buffer)))
+        # self.print("\r                                             ")
+        print("\r"+self.__promote+line_buffer+trailing_space_nmu*" ", end="", flush=True)
+        print("\033[%dD" % (cursor_shift_idx + trailing_space_nmu), end="", flush=True)
 
-    def parse(self):
-        pass
-    def regist_cmd(self, key_word, func_ptr, description=""):
-        self.__function_dict[key_word] = [func_ptr, description]
+    def regist_cmd(self, key_word, func_ptr, description="", arg_list=None):
+        self.__function_dict[key_word] = CommandInstance(key_word=key_word, func_ptr=func_ptr, description=description, arg_list=arg_list)
         # print(self.__function_dict)
     def run_once(self, line_args):
         func_ret = False
@@ -155,7 +193,7 @@ class CommandLineInterface:
                     (self.__auto_match is True and len(cmd_token) >= 4 and cmd_token[:4] == each_key[:4]):
                 dbg_debug("Cmd: ", line_buffer)
                 try:
-                    func_ret = self.__function_dict[each_key][0](arg_dict)
+                    func_ret = self.__function_dict[each_key].func_ptr(arg_dict)
                 except Exception as e:
 
                     dbg_error("Cmd: ", line_buffer)
@@ -165,14 +203,45 @@ class CommandLineInterface:
                     dbg_error(traceback_output)
 
                 return func_ret
-        self.ui_print_line("command not found")
+        self.print("command not found")
         return func_ret
     def run(self):
         while self.__flag_running == True:
             line_buffer=self.get_line()
             func_ret = self.run_once(line_buffer)
             if func_ret is not True:
-                self.ui_print_line('Fail to excute command. Return:', func_ret)
+                self.print('Fail to excute command. Return:', func_ret)
+    def __auto_complete(self, line_buffer):
+        candict_list = list()
+        # print('\n__auto_complete')
+        cmd_token = line_buffer.split(' ')
+        if len(cmd_token) == 1 and cmd_token[0] != '':
+            # print('auto compolete cmd', cmd_token, cmd_token[0])
+            pattern = cmd_token[0]
+            arg_list = self.__function_dict.keys()
+            for each_key in arg_list:
+                if each_key.startswith(pattern) is True:
+                    # print(each_key)
+                    candict_list.append(each_key)
+        elif len(cmd_token) > 1 and line_buffer.endswith(' ') is False:
+            # print('auto compolete args', cmd_token, cmd_token[0], cmd_token[-1])
+            pattern = cmd_token[-1]
+            command = cmd_token[0]
+            if command in self.__function_dict.keys() and self.__function_dict[command].arg_list is not None:
+                arg_list = self.__function_dict[command].arg_list
+            else:
+                arg_list = list()
+            # print('arg_list',arg_list)
+
+            for each_key in arg_list:
+                if each_key.startswith(pattern) is True:
+                    # print(each_key)
+                    candict_list.append(each_key)
+        # else:
+        #     print('No need to complete')
+
+        # print(candict_list)
+        return candict_list
 
     def get_line(self):
         ckey_timestatmp=time.time()
@@ -187,9 +256,7 @@ class CommandLineInterface:
         history_idx=0
         buffer_cusor_idx=0
 
-        # self.ui_print(self.__promote)
-        self.ui_print("\r"+self.__promote+line_buffer)
-        # ui_print(self.__promote)
+        print("\r"+self.__promote+line_buffer, end='', flush=True)
         while self.__flag_running == True:
             pkey_timestatmp = ckey_timestatmp
             key_press = getch()
@@ -232,9 +299,27 @@ class CommandLineInterface:
                 #     tmp_idx=len(line_buffer)-(buffer_cusor_idx + 1)
 
 
-                # self.ui_print_line('')
-                # self.ui_print_line("Start: " + line_buffer[:tmp_idx])
-                # self.ui_print_line("End: " + line_buffer[tmp_idx:] )
+                # self.print('')
+                # self.print("Start: " + line_buffer[:tmp_idx])
+                # self.print("End: " + line_buffer[tmp_idx:] )
+                self.__print_line_buffer(line_buffer, buffer_cusor_idx)
+                continue
+            elif key_press == chr(0x09):
+                # candict_list = self.__auto_complete(line_buffer)
+                center_idx = len(line_buffer) - buffer_cusor_idx
+                front_buffer = line_buffer[:center_idx]
+                post_buffer = line_buffer[center_idx:]
+                # print('\n'+ 'cmd ->' + front_buffer + ' - ' + post_buffer + '\n')
+
+                candict_list = self.__auto_complete(front_buffer)
+                # print(candict_list)
+                if len(candict_list) == 1:
+                    last_arg = front_buffer.split(' ')[-1]
+                    line_buffer = front_buffer + candict_list[0][len(last_arg):] + post_buffer
+                    # print('\n',candict_list)
+                elif len(candict_list) > 1:
+                    print('\n', candict_list)
+
                 self.__print_line_buffer(line_buffer, buffer_cusor_idx)
                 continue
             # elif key_press.encode("ascii") == b'\x1f':
@@ -246,25 +331,18 @@ class CommandLineInterface:
                 elif pkey_press == '[':
                     if key_press == 'D':
                         # return('left')
-                        # self.ui_print_line("Cursor Idx: ",buffer_cusor_idx)
+                        # self.print("Cursor Idx: ",buffer_cusor_idx)
                         if len(line_buffer) >= buffer_cusor_idx + 1:
                             buffer_cusor_idx=buffer_cusor_idx + 1
                             self.__print_line_buffer(line_buffer, buffer_cusor_idx)
-                        # elif 1 == buffer_cusor_idx:
-                        #     buffer_cusor_idx=buffer_cusor_idx + 1
-                        #     self.ui_print("\r"+self.__promote+line_buffer)
-                        #     self.ui_print("\033[%dD" % (buffer_cusor_idx))
                     elif key_press == 'C':
                         # return('right')
-                        # self.ui_print_line("Cursor Idx: ",buffer_cusor_idx)
+                        # self.print("Cursor Idx: ",buffer_cusor_idx)
                         if 1 <= buffer_cusor_idx:
                             buffer_cusor_idx=buffer_cusor_idx - 1
                             self.__print_line_buffer(line_buffer, buffer_cusor_idx)
-                        # elif 1 == buffer_cusor_idx:
-                        #     buffer_cusor_idx=buffer_cusor_idx - 1
-                        #     self.__print_line_buffer(line_buffer, buffer_cusor_idx)
                     elif key_press == 'A':
-                        # self.ui_print_line("Hist Idx: ",history_idx)
+                        # self.print("Hist Idx: ",history_idx)
                         # return('up')
                         if 0 == history_idx:
                             line_bakup_buffer = line_buffer
@@ -274,7 +352,7 @@ class CommandLineInterface:
                             line_buffer=self.__history_list[-history_idx]
                         self.__print_line_buffer(line_buffer, buffer_cusor_idx)
                     elif key_press == 'B':
-                        # self.ui_print_line("Hist Idx: ",history_idx)
+                        # self.print("Hist Idx: ",history_idx)
                         # return('down')
                         if 1 < history_idx:
                             history_idx=history_idx - 1
@@ -294,9 +372,9 @@ class CommandLineInterface:
                             line_buffer = line_buffer[:tmp_idx] + line_buffer[tmp_idx + 1:]
                             buffer_cusor_idx = buffer_cusor_idx - 1
 
-                        # self.ui_print_line('')
-                        # self.ui_print_line("Start: " + line_buffer[:tmp_idx])
-                        # self.ui_print_line("End: " + line_buffer[tmp_idx:] )
+                        # self.print('')
+                        # self.print("Start: " + line_buffer[:tmp_idx])
+                        # self.print("End: " + line_buffer[tmp_idx:] )
                         self.__print_line_buffer(line_buffer, buffer_cusor_idx)
                     elif key_press == '1':
                         # FIXME This is not real key, only detect first 3 code
@@ -323,7 +401,7 @@ class CommandLineInterface:
             # normal key press
             if key_press in ("\r", "\n"):
                 # print("Enter")
-                self.ui_print("\n")
+                print("\n", end='', flush=True)
                 break
             else:
                 # update buffer
@@ -338,37 +416,26 @@ class CommandLineInterface:
                 self.__print_line_buffer(line_buffer, buffer_cusor_idx)
                 continue
         self.__history_list.append(line_buffer)
-        # self.ui_print_line(self.__history_list)
-        # self.ui_print_line(self.line_buffer)
+        # self.print(self.__history_list)
+        # self.print(self.line_buffer)
         return line_buffer
-    # def start_thread(self):
-    #     x = threading.Thread(target=self.key_press, args=(0,))
-    #     x.start()
-    #     self.ui_print_line("End of init key thread")
-
-    # def key_press(self, test):
-    #     while True:
-    #         x_tmp = getch()
-    #         self.ui_print_line("KeyPress: {}".format(x_tmp))
-    #         time.sleep(0.05)
-    #         if x_tmp in ("q", "Q", "x", "X"):
-    #             # self.ui_print_line(psettings.get('msg_exit'))
-    #             return
 
 def args_test_function(args):
-    print("cmd: ", args)
+    CommandLineInterface.print("cmd: ", args)
     if  len(args) == 1:
-        print("Only one args")
+        dbg_info("Only one args, replace by test pattern")
         # args = "add task project:test_project_name name:test_task_name description:'desc about task' start:today due:eow"
         # args = "add task project:test_project_name name:test_task_name"
         args = "add task project:test_project_name"
     args_parser = ArgParser(args = args)
     for each_key in args_parser.keys():
-        print(each_key ,":", args_parser[each_key])
+        CommandLineInterface.print(each_key ,":", args_parser[each_key])
+
+    return True
 
 if __name__ == '__main__':
     debug_level=DebugLevel.MAX
     test_cli = CommandLineInterface()
     test_cli.set_mode(EUIMode.INTERCTIVE)
-    test_cli.regist_cmd("args", args_test_function, description="test function for args")
+    test_cli.regist_cmd("test", args_test_function, description="test function for args", arg_list=['project', 'task', 'name', 'description']  )
     test_cli.run()
